eb826b70ea7fa759b5134890da8aa0d988865d07
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-07-15 22:11:44 -0300

    Global styles

---
+++ src/styles/global.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-image: linear-gradient(#000, #303030, #000);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px Roboto, sans-serif;
+  }
+
+  #root {
+    height: 100vh;
+    max-width: 1020px;
+    margin: 0 auto;
+    padding: 0 20px 50px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
