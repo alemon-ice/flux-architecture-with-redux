1f8e03f5842f8d7efb064a0b2e477d2cd786e0e7
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-07-18 23:40:24 -0300

    Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-// import { MdShoppingBasket } from 'react-icons/md';
+import { MdShoppingBasket } from 'react-icons/md';
 
 import { Container, Cart } from './styles';
 
 // import logo from '../../assets/images/logo.png';
 
 export default function Header() {
-  // const cartSize = useSelector((state) => state.cart.length);
+  const cartSize = useSelector((state) => state.cart.length);
 
   return (
     <Container>
       <Link to="/">
-        <h2>Shopping Cart</h2>
+        <h2>Redux Store</h2>
       </Link>
 
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
-          {/* <span>{cartSize} itens</span> */}
+          <span>{cartSize} itens</span>
         </div>
-        {/* <MdShoppingBasket size={36} color="#FFF" /> */}
+        <MdShoppingBasket size={36} color="#FFF" />
       </Cart>
     </Container>
   );
